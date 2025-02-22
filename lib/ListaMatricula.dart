f90f859214645f322437b1edee67aae67f33a798
Jordy-Pedro-Valencia-Jara <jquispejar@unsa.edu.pe>
2025-02-21 22:54:29 -0500

    crear buscador

--- lib/ListaMatricula.dart
+++ lib/ListaMatricula.dart
@@ -3,8 +3,56 @@ import 'package:flutter/material.dart';
 class ListaMatricula extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      home:vistalista(),
+    );
+  }
+}
+
+class vistalista extends StatefulWidget{
+  @override
+  _vistalista createState()=> _vistalista();
+}
+
+class _vistalista extends State<vistalista>{
+  
+
+  // Controlador de busqueda
+  TextEditingController buscadorcontroller = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("funciona"),),
+      appBar: AppBar(
+        title: Text("Buscar "),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            //Crea el espacio para realizar la busqueda
+            child: TextField(
+              controller: buscadorcontroller,
+              decoration: InputDecoration(
+                hintText: "Buscar...",
+                border: OutlineInputBorder(),
+              ),
+              
+            ),
+          
+          ),
+          Expanded(
+            child: 
+            ListView.builder(
+              itemBuilder: (context,index) {
+
+
+                },
+            ),
+          ),
+        ],
+      ),
     );
   }
 
